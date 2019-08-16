be7a50c9f1837a569304677b42d8e6ca0d5c293e
Victor Berland <vb@victorberland.com>
2019-08-16 13:49:19 +0200

    fyrste

---
+++ README.md
@@ -0,0 +1,11 @@
+# .V
+## punktfiler - dotfiles
+
+`mkdir ~/.v/ && cd ~/.v`
+`sh i.sh`
+Vim: `:PlugInstall`
+
+
+
+- [Bashdot](https://github.com/bashdot/bashdot)
+
---
+++ i.sh
@@ -0,0 +1,25 @@
+
+
+cd ~
+
+# bashdot
+curl -s https://raw.githubusercontent.com/bashdot/bashdot/master/bashdot > /tmp/bashdot
+sudo mv /tmp/bashdot /usr/local/bin
+sudo chmod a+x /usr/local/bin/bashdot
+
+
+# Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Oh My ZSH
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
+# ZSH Common
+wget -O $ZSH_CUSTOM/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
+
+git clone https://github.com/victorberland/.v.git
+cd .v
+
+chsh -s /bin/zsh
+bashdot install v
